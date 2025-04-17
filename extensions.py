d218a00e437e9c9f730bdf91e5413b4b964d8c97
Dunkrick <rithwickgurram05@gmail.com>
2025-04-17 19:37:33 +0530

    Fix: Update dependencies and deployment configuration

--- extensions.py
+++ extensions.py
@@ -4,14 +4,12 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask_caching import Cache
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
 csrf = CSRFProtect()
 limiter = Limiter(key_func=get_remote_address)
-cache = Cache()
 
 # Configure login manager
 login_manager.login_view = 'auth.login'
@@ -20,5 +18,5 @@ login_manager.login_message_category = 'info'
 
 @login_manager.user_loader
 def load_user(user_id):
-    from models import User  # Import here to avoid circular imports
+    from models import User
     return User.query.get(int(user_id))
